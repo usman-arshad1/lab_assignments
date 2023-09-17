e4cdcde59d1599d4019e279b960705d20246c649
Usman Arshad <uarshad2012@gmail.com>
2023-09-17 15:16:47 -0700

    deploying for hosting

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+
+app.get('/', (req,res) => { 
+    res.sendFile("index.html", { root: __dirname })
+}); 
+
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+	console.log("Listening on port " + port);
+}); 

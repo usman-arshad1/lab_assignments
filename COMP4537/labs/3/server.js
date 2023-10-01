8dc93db333ece4f28aad29797adece986b4c4655
Usman Arshad <uarshad2012@gmail.com>
2023-10-01 15:56:07 -0700

    updated code

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -3,13 +3,15 @@ const http = require('http');
 const url = require('url');
 
 http.createServer((req, res) => {
-    let param = url.parse(req.url, true).query["name"];
-    if(param == undefined) {
-        param = "Usman";
-    };
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(`<font color="blue">Hello ${param}, What a beautiful day. Server current date and time is ${utils.date()}</font>`);
-    res.end();
+    if (url.parse(req.url,true).pathname == "/COMP4537/labs/3/getDate/"){
+        let param = url.parse(req.url, true).query["name"];
+        if(param == undefined) {
+            param = "Usman";
+        };
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(`<font color="blue">Hello ${param}, What a beautiful day. Server current date and time is ${utils.date()}</font>`);
+        res.end();
+    }
 }).listen(3000);    
 
 console.log(`Server is listening on port 3000`);

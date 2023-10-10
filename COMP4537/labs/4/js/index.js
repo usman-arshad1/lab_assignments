3096f90073de37939e0ce18d1b68a5ac04a578ea
Usman Arshad <uarshad2012@gmail.com>
2023-10-09 22:50:26 -0700

    adding comments

--- COMP4537/labs/4/js/index.js
+++ COMP4537/labs/4/js/index.js
@@ -1,3 +1,4 @@
+//Used for getting the input in the word search
 document.getElementById('searchForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const query = document.getElementById('searchInput').value.toLowerCase();
@@ -7,7 +8,7 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
         clearResults();
     }
 });
-
+//Used in the adding word to the dictionary
 document.getElementById('addForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const word = document.getElementById('addWordInput').value.toLowerCase();
@@ -17,17 +18,24 @@ document.getElementById('addForm').addEventListener('submit', function(event) {
     }
 });
 
-
+//The post request to search for the word in the API backend
 function searchWord(word) {
+    //Creat a new XMLHttpRequest from the backend
     const xhr = new XMLHttpRequest();
+    //Open a connection to the api using the get for searching a word
     xhr.open('GET', `https://kcawvnqseh.us14.qoddiapp.com/api/definitions?word=${word}`, true);
+    //Sends the above request to the backend
     xhr.send();
+    //Returns the request based on the ready state and status of the request from the backend server
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
+            //take the returned data and store it in a variable 
             let data = JSON.parse(xhr.responseText)
+            //Add the data to the div for the results
             document.getElementById('results').innerHTML = 
                 data.response + ": " + data.word + ": " + data.definition
         } else {
+            //Else for if returns an error like word doesnt exist
             let data = JSON.parse(xhr.responseText)
             document.getElementById('results').innerHTML = 
                 data.response + ": " + data.error
@@ -37,17 +45,25 @@ function searchWord(word) {
 }
 
 function addDefinition(word, definition) {
+    //Creat a new XMLHttpRequest from the backend
     const xhr = new XMLHttpRequest();
+    //Turn the word and definition into JSON format
     let sendingData = {word: `${word}`, definition: `${definition}`}
+    //Make them a JSON and put in data variable to send later in the body of the POST
     let data = JSON.stringify(sendingData);
+    //Open a connection to the api using POST for adding a word in the body
     xhr.open('POST', 'https://kcawvnqseh.us14.qoddiapp.com/api/definitions', true);
+    //Send data to backend
     xhr.send(data);
+    //Check the ready state response from the backend
     xhr.onreadystatechange = function() {
+        //If the word saves does this part with a return of the data from the server and adds to results
         if (xhr.readyState === 4 && xhr.status === 200) {
             let info = JSON.parse(this.responseText)
                 document.getElementById('results').innerHTML = 
                     info.response + ": " + info.message;
         } else {
+            //If definition in the dictionary than response of error in the results box
             let info = JSON.parse(this.responseText)
             document.getElementById('results').innerHTML = 
                 info.response + ": " + info.error;
@@ -55,6 +71,7 @@ function addDefinition(word, definition) {
     };
 }
 
+//function for clearing the reults div if you search any word shorter than 3 characters.
 function clearResults() {
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '';
